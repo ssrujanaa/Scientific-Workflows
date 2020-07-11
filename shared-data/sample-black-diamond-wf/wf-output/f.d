4c7cc39e806789e5de0b846aa2d276933004f9da
Srujana Subramanya <36698999+ssrujanaa@users.noreply.github.com>
2020-07-10 20:51:03 -0400

    wf

---
+++ shared-data/sample-black-diamond-wf/wf-output/f.d
@@ -0,0 +1,17 @@
+  --- start f.c1 ----
+  --- start f.b1 ----
+  --- start f.a ----
+This is sample input to KEG--- final f.a ----
+IP addr and hostname: 172.17.0.2 (VPN)
+--- final f.b1 ----
+ÿÿÿÿÿÿAIP addr and hostname: 172.17.0.2 (VPN)
+--- final f.c1 ----
+--- start f.c2 ----
+  --- start f.b2 ----
+  --- start f.a ----
+This is sample input to KEG--- final f.a ----
+IP addr and hostname: 172.17.0.2 (VPN)
+--- final f.b2 ----
+ÿÿÿÿÿÿAIP addr and hostname: 172.17.0.2 (VPN)
+--- final f.c2 ----
+IP addr and hostname: 172.17.0.2 (VPN)
