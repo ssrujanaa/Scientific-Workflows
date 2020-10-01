aecbc5a852349a687f2b402b809e229d99f156bd
Srujana Subramanya <36698999+ssrujanaa@users.noreply.github.com>
2020-10-01 02:41:11 -0700

    update

--- shared-data/Scientific-Workflows-master 3/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker container run \
-    -p 8888:8888 \
-    --privileged \
-    --mount type=bind,source="$(pwd)"/shared-data,target=/home/scitech/shared-data \
-    ryantanaka/pegasus5-condor
